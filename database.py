bf09da1734abaa4bff9968a51dae27cf82603602
Sujisha M A <85470299+sujishasuresh@users.noreply.github.com>
2026-07-07 23:16:47 +0530

    Change database connection password to placeholder
    Updated database connection password placeholder for security.

--- database.py
+++ database.py
@@ -1,8 +1,4 @@
 import pymysql
 
-conn=pymysql.connect(host="localhost",user="root",password="Sujisha03@",database="student_management",cursorclass=pymysql.cursors.DictCursor)
+conn=pymysql.connect(host="localhost",user="root",password="YOUR_PASSWORD",database="student_management",cursorclass=pymysql.cursors.DictCursor)
 curr=conn.cursor()
-# sql1="insert into student values(1,'Taylor','Female','2000-01-15','tayalor20@gmail.com')"
-# curr.execute(sql1)
-# conn.commit()
-# conn.close()
